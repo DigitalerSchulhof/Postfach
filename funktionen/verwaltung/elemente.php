c8ed1b6d929388f82e7d9667b5a672cc5628cbef
jeengbe <jesper.engberg@gmx.at>
2020-08-21 12:36:45 +0200

    [Fix] Demo-Recht in Verwaltungselemente

--- funktionen/verwaltung/elemente.php
+++ funktionen/verwaltung/elemente.php
@@ -6,5 +6,5 @@ use UI\Icon;
 
 $essen    = Liste::addKategorie(new \Kern\Verwaltung\Kategorie("essen", "Essen"));
 
-$essen[] = new Element("Lasagne", "Lasagnegehalt einstellen", new Icon("fas fa-cat"), "Schulhof/Verwaltung/Garfield", true);
+if($DSH_BENUTZER->hatRecht("lecker") || false)  $essen[] = new Element("Lasagne", "Lasagnegehalt einstellen", new Icon("fas fa-cat"), "Schulhof/Verwaltung/Garfield", true);
 ?>
